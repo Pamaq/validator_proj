0c4057fea570589709f8d62f432e4fdbeb945eb4
Pamaq <paulinakuta00@gmail.com>
2022-12-29 11:20:54 +0100

    Added validation of username and email

--- script.js
+++ script.js
@@ -26,26 +26,49 @@
 
 // second form
 const username = document.getElementById("username");
+const first = document.getElementById("first");
+const second = document.getElementById("second");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
-const passwordcheck = document.getElementById("passwordcheck");
+const passwordCheck = document.getElementById("passwordcheck");
 const usernameError = document.getElementById("usernameError");
+const emailError = document.getElementById("emailError");
 const submit = document.querySelector("button");
 
 submit.addEventListener("click", () => {
+	checkUsername();
+	checkEmail();
+});
+
+const checkUsername = () => {
 	if (username.value === "" || username.value === null) {
 		usernameError.style.visibility = "visible";
-		// document.getElementsByClassName("fa-exclamation-circle").style.visibility =
-		// 	"visible";
 		usernameError.textContent = "Username is required!";
-	}
-	if (username.value.length > 0 && username.value.length < 7) {
+		first.classList.add("error");
+	} else if (username.value.length > 0 && username.value.length < 7) {
 		usernameError.style.visibility = "visible";
 		usernameError.textContent = "Username must be at least 7 characters long!";
+		first.classList.add("error");
+	} else {
+		first.classList.add("success");
+		first.classList.remove("error");
+		usernameError.style.visibility = "hidden";
 	}
-});
-// form.addEventListener("submit", (e) => {
-// 	if (username.value === "" || username.value === null) {
-// 		usernameError.value = "Name is required!";
-// 	}
-// });
+};
+
+const checkEmail = () => {
+	const asperand = "@";
+	if (email.value === "" || email.value === null) {
+		emailError.style.visibility = "visible";
+		emailError.textContent = "Email is required!";
+		second.classList.add("error");
+	} else if (email.value.includes(asperand) == 0) {
+		emailError.style.visibility = "visible";
+		emailError.textContent = "Wrong formula!";
+		second.classList.add("error");
+	} else {
+		second.classList.add("success");
+		second.classList.remove("error");
+		emailError.style.visibility = "hidden";
+	}
+};
